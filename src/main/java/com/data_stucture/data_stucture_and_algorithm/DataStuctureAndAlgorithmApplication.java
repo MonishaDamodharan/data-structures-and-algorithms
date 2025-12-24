9edd7e27958d88dcad8eba54db2ce224c6a2f2a7
Monisha <missmonishavimala@gmail.com>
2025-12-24 19:19:18 +0530

    Base code setup

---
+++ src/main/java/com/data_stucture/data_stucture_and_algorithm/DataStuctureAndAlgorithmApplication.java
@@ -0,0 +1,13 @@
+package com.data_stucture.data_stucture_and_algorithm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStuctureAndAlgorithmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStuctureAndAlgorithmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/data_stucture/data_stucture_and_algorithm/DataStuctureAndAlgorithmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.data_stucture.data_stucture_and_algorithm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataStuctureAndAlgorithmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
